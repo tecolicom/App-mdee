20ca7ad05d40f5cee6ca8f71da8075b7d510f738
Kazumasa Utashiro <kaz@utashiro.com>
2026-01-16 22:32:15 +0900

    Initial commit

---
+++ cpanfile
@@ -0,0 +1,11 @@
+requires 'perl', 'v5.26';
+requires 'File::Share';
+requires 'Getopt::Long::Bash';
+requires 'App::Greple', '10.02';
+requires 'App::Greple::tee', '1.03';
+requires 'App::ansifold', '1.34';
+requires 'App::ansicolumn', '1.50';
+
+on 'test' => sub {
+    requires 'Test::More', '0.98';
+};
---
+++ lib/App/mdv.pm
@@ -0,0 +1,40 @@
+package App::mdv;
+
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+use Exporter 'import';
+our @EXPORT_OK = qw($VERSION);
+
+1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::mdv - mdv script dummy module
+
+=head1 SYNOPSIS
+
+    use App::mdv;
+
+=head1 DESCRIPTION
+
+Document is included in C<mdv> script file.
+
+=head1 LICENSE
+
+Copyright 2025 Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kazumasa Utashiro
+
+=cut
